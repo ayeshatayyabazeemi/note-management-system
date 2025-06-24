782159015b10ac1b68f581271be036f19292c6a2
Ayesha <ayeshatayyab027@gmail.com>
2025-06-24 19:48:41 +0500

    Initial commit: set up MERN stack

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// const authRoutes = require('./routes/authRoutes');
+// const noteRoutes = require('./routes/noteRoutes');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/notes', noteRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+.catch(err => console.error('MongoDB connection error:', err));
